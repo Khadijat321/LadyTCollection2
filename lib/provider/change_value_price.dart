a6370446da558815834a2fd7d1a4c1d96bec9fea
Olagunju Ganiyu Ayodele <117236179+HarlabE@users.noreply.github.com>
2026-09-25 17:04:22 +0100

    update the statemanagement to use provide instead of setstate

--- lib/provider/change_value_price.dart
+++ lib/provider/change_value_price.dart
@@ -1,18 +1,54 @@
 import 'package:flutter/material.dart';
 // import 'package:provider/provider.dart';
 class ChangeValuePrice extends ChangeNotifier {
-  double _valuePrice = 0.0;
+  List<Map<String, dynamic>> _products = [
+    {'name': 'Laptop', 'quantity': 0, 'price': 300000},
+    {'name': 'Phone', 'quantity': 0, 'price': 200000},
+    {'name': 'Tv', 'quantity': 0, 'price': 500000},
+  ];
 
-  double get valuePrice => _valuePrice;
+List<Map<String, dynamic>> get products => _products;
 
-  int price = 30000;
+ int get totalItem {
+    int total = 0;
+    for (var product in products) {
+      total += product['quantity'] as int;
+    }
 
-  void setValuePrice(double newValue) {
-    _valuePrice = newValue;
+    return total;
+  }
+
+  int get totalPrice {
+    int total = 0;
+    for (var product in products) {
+      total += product['price'] * product['quantity'] as int;
+    }
+
+    return total;
+  }
+
+  void addToCart(int index) {
+    
+    products[index]['quantity'] = (product[index]['quantity'] as int ) + 1 ;
     notifyListeners();
   }
-  void setLaptopPrice ( int quantity ){
-      price = price * quantity;
+
+  void removeFromCart(int index) {
+    if (products[index]['quantity'] > 0) {
+      products[index]['quantity'] = (product[index]['quantity'] as int ) - 1 ;
+  
+    }
       notifyListeners();
+  }
+
+ 
+
+  void clearCart() {
+    for (var product in products) {
+      
+        product['quantity'] = 0;
+      
+    }
+    notifyListeners();
   }
 }
--- lib/shopping_cart.dart
+++ lib/shopping_cart.dart
@@ -12,61 +12,19 @@ class Screen extends StatefulWidget {
 }
 
 class _ScreenState extends State<Screen> {
-  List<Map<String, dynamic>> products = [
-    {'name': 'Laptop', 'quantity': 0, 'price': 300000},
-    {'name': 'Phone', 'quantity': 0, 'price': 200000},
-    {'name': 'Tv', 'quantity': 0, 'price': 500000},
-  ];
-
-  void addToCart(int index) {
-    setState(() {
-      products[index]['quantity']++;
-    });
-  }
-
-  void removeFromCart(int index) {
-    if (products[index]['quantity'] > 0) {
-      setState(() {
-        products[index]['quantity']--;
-      });
-    }
-  }
-
-  int get totalItem {
-    int total = 0;
-    for (var product in products) {
-      total += product['quantity'] as int;
-    }
-
-    return total;
-  }
-
-  int get totalPrice {
-    int total = 0;
-    for (var product in products) {
-      total += product['price'] * product['quantity'] as int;
-    }
-
-    return total;
-  }
-
-  void clearCart() {
-    for (var product in products) {
-      setState(() {
-        product['quantity'] = 0;
-      });
-    }
-  }
+  
 
   @override
   Widget build(BuildContext context) {
+    final cart = context.watch<ChangeValuePrice>();
+    final readCart = context.read<ChangeValuePrice>()
     return Scaffold(
       appBar: AppBar(
         title: Text('Shopping Cart'),
         actions: [
           Padding(
             padding: EdgeInsetsGeometry.only(right: 20),
-            child: Text('Cart: $totalItem'),
+            child: Text('Cart: ${cart.totalItem.toString()}'),
           ),
         ],
       ),
@@ -76,32 +34,31 @@ class _ScreenState extends State<Screen> {
             child: ListView.builder(
               itemCount: products.length,
               itemBuilder: (_, index) {
-                final product = products[index];
+                final product = cart.products[index];
+                final int quantity = product['quantity'] as int; 
+                final int price = product['price'] as int; 
+                final int totalPrice = price * quantity;
+
                 return Card(
                   margin: EdgeInsets.all(20),
                   child: ListTile(
                     title: Text(product['name']),
                     subtitle: Text(
-                      product['name'] == 'Laptop'
-                          ? context.watch<ChangeValuePrice>().price.toString()
-                          : product['price'].toString(),
+                      price.toString(),
                     ),
                     trailing: Row(
                       mainAxisSize: MainAxisSize.min,
                       children: [
                         IconButton(
                           onPressed: () {
-                            removeFromCart(index);
+                            readCart.removeFromCart(index);
                           },
                           icon: Icon(Icons.remove),
                         ),
                         Text(product['quantity'].toString()),
                         IconButton(
                           onPressed: () {
-                            addToCart(index);
-                            context.read<ChangeValuePrice>().setLaptopPrice(
-                              product['quantity'],
-                            );
+                           readCart.addToCart(index);
                           },
                           icon: Icon(Icons.add),
                         ),
@@ -118,7 +75,7 @@ class _ScreenState extends State<Screen> {
               children: [
                 Text('Total Item: $totalItem'),
                 Text('Total Price: $totalPrice'),
-                ElevatedButton(onPressed: clearCart, child: Text('Clear Cart')),
+                ElevatedButton(onPressed: readCart.clearCart, child: Text('Clear Cart')),
               ],
             ),
           ),
